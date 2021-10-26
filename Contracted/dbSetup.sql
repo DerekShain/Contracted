5258854513ab56b829d429425b86b122cf7c2c04
Derek Shain <derek0anthony@gmail.com>
2021-10-26 14:13:04 -0600

    init2

--- Contracted/dbSetup.sql
+++ Contracted/dbSetup.sql
@@ -6,3 +6,19 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
+CREATE TABLE IF NOT EXISTS company(
+  id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL COMMENT 'Company Name'
+)default charset utf8 COMMENT '';
+
+CREATE TABLE IF NOT EXISTS contractor(  
+    id int NOT NULL primary key AUTO_INCREMENT,
+    name varchar(255) NOT NULL COMMENT 'Contractor Name',
+    picture varchar(255) Comment'Contractor Picture'
+) default charset utf8 COMMENT '';
+
+CREATE TABLE IF NOT EXISTS job(
+id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+contractorId int NOT NULL,
+companyId int NOT NULL
+) default charset utf8 COMMENT '';
